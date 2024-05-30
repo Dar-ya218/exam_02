c8c0934e42a36dfd5fada7ec03ff1e7fbc55aefa
dabochko <dabochko@student.42barcelona.com>
2024-05-30 14:19:30 +0200

    putstr

--- 1_lev/strcpy/ft_strcpy.c
+++ 1_lev/strcpy/ft_strcpy.c
@@ -10,3 +10,15 @@ char	*ft_strcpy(char *s1, char *s2)
 	s1[i] = '\0';
 	return (s1);
 }
+int main()
+{
+    char original[] = "Hola Mundo";
+    char copia[30];
+
+    ft_strcpy(copia, original);
+
+    printf("Original: %s\n", original);
+    printf("Copia: %s\n", copia);
+
+    return 0;
+}
